386a4af29d7b0f2e11dbee8d0be418645e14fdf5
Jonas Souza <jonasmzsouza@gmail.com>
2025-01-23 01:31:09 -0300

    feat(ses): remove some parameters

--- modules/ses/parameters.tf
+++ modules/ses/parameters.tf
@@ -1,26 +1,12 @@
-# Store the SES email identity ID in the Parameter Store
-resource "aws_ssm_parameter" "ses_identity_id" {
-  name  = "/ses/ses_email_identity_id"
-  type  = "String"
-  value = aws_ses_email_identity.ses_email_identity.id
-}
-
 # Store the SES email identity ARN in the Parameter Store
-resource "aws_ssm_parameter" "ses_identity_arn" {
+resource "aws_ssm_parameter" "ses_email_identity_arn" {
   name  = "/ses/ses_email_identity_arn"
   type  = "String"
   value = aws_ses_email_identity.ses_email_identity.arn
 }
 
-# Store the SES domain identity ID in the Parameter Store
-resource "aws_ssm_parameter" "ses_identity_id" {
-  name  = "/ses/ses_domain_identity_id"
-  type  = "String"
-  value = aws_ses_domain_identity.ses_domain_identity.id
-}
-
 # Store the SES domain identity ARN in the Parameter Store
-resource "aws_ssm_parameter" "ses_identity_arn" {
+resource "aws_ssm_parameter" "ses_domain_identity_arn" {
   name  = "/ses/ses_domain_identity_arn"
   type  = "String"
   value = aws_ses_domain_identity.ses_domain_identity.arn
