10f8a0e0aa2e6dc7f02392c5b1a7bcc079b8d260
Jonas Souza <jonasmzsouza@gmail.com>
2025-02-03 23:14:57 -0300

    fix: rename domain variables outputs

--- src/infra/shared/ses/outputs.tf
+++ src/infra/shared/ses/outputs.tf
@@ -11,13 +11,13 @@ output "ses_email_identity_arn" {
 }
 
 # SES domain identity ID
-output "ses_email_identity_id" {
+output "ses_domain_identity_id" {
   description = "SES domain identity ID"
   value       = aws_ses_domain_identity.ses_domain_identity.id
 }
 
 # SES domain identity ARN
-output "ses_email_identity_arn" {
+output "ses_domain_identity_arn" {
   description = "SES domain identity ARN"
   value       = aws_ses_domain_identity.ses_domain_identity.arn
 }
