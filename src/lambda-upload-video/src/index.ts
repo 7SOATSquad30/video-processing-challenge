5f81f9be839413c6986e988376298948b36e85ea
Murilo Kakazu <yuri.kakazu@gmail.com>
2025-02-09 16:28:52 -0300

    feat: use user data from jwt

--- src/lambda-upload-video/src/index.ts
+++ src/lambda-upload-video/src/index.ts
@@ -1,43 +1,47 @@
-import { UploadVideoRequest, Video, VideoProcessingStatus } from "./model";
+import { UploadVideoRequest, UserData, Video, VideoProcessingStatus } from "./model";
 import { validateVideoWasUploaded } from "./storage.service";
 import { saveVideo } from "./video.repository";
 import { notifyNewVideoToBeProcessed } from "./messaging.service";
+import jwt from 'jsonwebtoken';
 
 exports.handler = async (event: any) => {
+    console.log('Event:', event);
+
+    const token = event.headers.Authorization.split(" ")[1];
+    const userData = <UserData | undefined> jwt.decode(token, { complete: true })?.payload;
+    if (!userData?.name || !userData?.email) {
+      return {
+        statusCode: 401,
+        body: JSON.stringify({
+          message: 'Invalid token',
+        }),
+      };
+    }
+
     try {
-        console.log('Event:', event);
-        
-        const userId = event?.pathParameters?.userId || event?.userId;
-        if (!userId) {
-          return {
-            statusCode: 400,
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ error: 'Field usedId is required.' })
-          };
-        }
+      const request: UploadVideoRequest = JSON.parse(event.body);
 
-        const request: UploadVideoRequest = JSON.parse(event.body);
+      const uploadTimestamp = new Date().getTime();
+      const video: Video = {
+        userId: userData.email,
+        userEmail: userData.email,
+        videoId: request.videoFileName,
+        status: VideoProcessingStatus.ENQUEUED,
+        s3ObjectKey: `${userData.email}_${request.videoFileName}`,
+        timestamp: uploadTimestamp,
+      };
 
-        const uploadTimestamp = new Date().getTime();
-        const video: Video = {
-          userId,
-          videoId: request.videoFileName,
-          status: VideoProcessingStatus.ENQUEUED,
-          s3ObjectKey: `${userId}_${request.videoFileName}`,
-          timestamp: uploadTimestamp,
-        };
+      await validateVideoWasUploaded(video.s3ObjectKey);
+      await saveVideo(video);
+      await notifyNewVideoToBeProcessed(video);
 
-        await validateVideoWasUploaded(video.s3ObjectKey);
-        await saveVideo(video);
-        await notifyNewVideoToBeProcessed(video);
+      const message = `Video ${request.videoFileName} upload was completed for user ${userData.email}.`;
+      console.log(message);
 
-        const message = `Video ${request.videoFileName} upload was completed for user ${userId}.`;
-        console.log(message);
-
-        return {
-            statusCode: 200,
-            body: JSON.stringify({ message })
-        };
+      return {
+          statusCode: 200,
+          body: JSON.stringify({ message })
+      };
     } catch (error: any) {
       console.error('Error', error);
       return {
